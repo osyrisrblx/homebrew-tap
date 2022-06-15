3a1e26752583d8751dd0beb7ff5696e1ff1a1641
osyrisrblx <osyrisrblx@gmail.com>
2022-06-14 22:08:26 -0400

    Attempt to pass syntax checks..

--- Formula/aftman.rb
+++ Formula/aftman.rb
@@ -1,10 +1,10 @@
 class Aftman < Formula
-  desc "Aftman, the prodigal sequel to Foreman"
+  desc "The prodigal sequel to Foreman"
   homepage "https://github.com/LPGhatguy/aftman"
   license "MIT"
-  version "0.2.3"
 
   url "https://github.com/LPGhatguy/aftman/releases/download/v0.2.3/aftman-0.2.3-linux-x86_64.zip"
+  version "0.2.3"
   sha256 "4228b29bcf09dbb46d0b4a68de167e66f589dceb8e4d7d72b6064ef3947bdb26"
 
   def install
@@ -12,6 +12,6 @@ class Aftman < Formula
   end
 
   test do
-    system "#{bin}/aftman --version"
+    system "#{bin}/aftman", "--version"
   end
 end
