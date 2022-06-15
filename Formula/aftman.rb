431f8de2d8451c21f246ba418f96dc324d9f0009
osyrisrblx <osyrisrblx@gmail.com>
2022-06-14 22:11:00 -0400

    Attempt to pass syntax checks..

--- Formula/aftman.rb
+++ Formula/aftman.rb
@@ -1,11 +1,11 @@
 class Aftman < Formula
-  desc "The prodigal sequel to Foreman"
+  desc "Prodigal sequel to Foreman"
   homepage "https://github.com/LPGhatguy/aftman"
-  license "MIT"
 
   url "https://github.com/LPGhatguy/aftman/releases/download/v0.2.3/aftman-0.2.3-linux-x86_64.zip"
-  version "0.2.3"
   sha256 "4228b29bcf09dbb46d0b4a68de167e66f589dceb8e4d7d72b6064ef3947bdb26"
+  version "0.2.3"
+  license "MIT"
 
   def install
     bin.install "aftman"
--- Formula/run-in-roblox.rb
+++ Formula/run-in-roblox.rb
@@ -3,11 +3,11 @@ require "fileutils"
 class RunInRoblox < Formula
   desc "Run a place, model, or individual script inside Roblox Studio"
   homepage "https://github.com/rojo-rbx/run-in-roblox"
-  license "MIT"
 
   url "https://github.com/rojo-rbx/run-in-roblox/releases/download/v0.3.0/run-in-roblox-0.3.0-win64.zip"
-  version "0.3.0"
   sha256 "590738bf134c0a1d3d6f865089bec8288fe7b7ec388471780f1f48b22d12440f"
+  version "0.3.0"
+  license "MIT"
 
   def install
     bin.install "run-in-roblox.exe" => "run-in-roblox"
@@ -15,7 +15,7 @@ class RunInRoblox < Formula
 
   # not sure why this is necessary, but run-in-roblox becomes readonly in the cellar
   def post_install
-    FileUtils.chmod("+x", "#{bin}/run-in-roblox")
+    chmod "+x", "#{bin}/run-in-roblox"
   end
 
   test do
