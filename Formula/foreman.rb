6b3c2860c8ebf4e1edd4f54f1f2c35349db4575c
osyrisrblx <osyrisrblx@gmail.com>
2023-09-30 14:18:18 -0400

    aftman -> foreman

---
+++ Formula/foreman.rb
@@ -0,0 +1,17 @@
+class Foreman < Formula
+  desc "Toolchain manager for Roblox projects"
+  homepage "https://github.com/Roblox/foreman"
+
+  url "https://github.com/Roblox/foreman/releases/download/v1.2.0/foreman-linux.zip"
+  version "1.2.0"
+  # sha256 ""
+  license "MIT"
+
+  def install
+    bin.install "foreman"
+  end
+
+  test do
+    system "#{bin}/foreman", "--version"
+  end
+end
