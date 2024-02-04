e71b300bdc6d3655ac4f718181c92ac390c9ef05
osyrisrblx <osyrisrblx@gmail.com>
2024-02-03 23:43:35 -0500

    Update foreman.rb

--- Formula/foreman.rb
+++ Formula/foreman.rb
@@ -2,8 +2,8 @@ class Foreman < Formula
   desc "Toolchain manager for Roblox projects"
   homepage "https://github.com/Roblox/foreman"
 
-  url "https://github.com/Roblox/foreman/releases/download/v1.2.0/foreman-linux.zip"
-  version "1.2.0"
+  url "https://github.com/Roblox/foreman/releases/download/v1.5.0/foreman-linux.zip"
+  version "1.5.0"
   # sha256 ""
   license "MIT"
 
